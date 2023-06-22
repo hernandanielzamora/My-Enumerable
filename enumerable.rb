eea07728e46ccc4b71edde1f175fe7f68944b3b5
Hernan Zamora <hernanzamora1002@gmail.com>
2023-06-22 14:19:00 -0300

    Deleted enumerable module content

--- enumerable.rb
+++ enumerable.rb
@@ -1,17 +1,3 @@
 module Enumerable
-  def all?
-    each { |item| return false unless yield item }
-    true
-  end
 
-  def any?
-    each { |item| return true if yield item }
-    false
-  end
-
-  def filter
-    result = []
-    each { |item| result.push(item) if yield item }
-    result
-  end
 end
